6329847c872c53e7bf6da9486111574c0479e49d
charmosaa <272682@student.pwr.edu.pl>
2024-12-18 22:54:05 +0100

    added: bootstrap view for store

--- DotNetLab10/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ DotNetLab10/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22dc16b9d33ee61cd7fedb2839e03b72febe90f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ae0abc09da3282b9f29e2d1e72de221194984c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
