d4e41ae688218016e1e2220ec47b66e429ce3310
charmosaa <272682@student.pwr.edu.pl>
2025-01-01 21:54:59 +0100

    adding and removing from cart

--- DotNetLab10/WebApplication1/Controllers/StoreController.cs
+++ DotNetLab10/WebApplication1/Controllers/StoreController.cs
@@ -24,4 +24,93 @@ public class StoreController : Controller
 
         return View(articles);
     }
+
+    public IActionResult AddToCart(int articleId)
+    {
+        string cookieName = $"article{articleId}";
+        int quantity = 1;
+
+        if (Request.Cookies.ContainsKey(cookieName))
+        {
+            if (int.TryParse(Request.Cookies[cookieName], out int currentQuantity))
+            {
+                quantity = currentQuantity + 1;
+            }
+        }
+
+        Response.Cookies.Append(cookieName, quantity.ToString(), new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(7)
+        });
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Cart()
+    {
+        var cartItems = new List<(WebApplication1.Models.Article Article, int Quantity)>();
+
+        foreach (var cookie in Request.Cookies)
+        {
+            if (cookie.Key.StartsWith("article"))
+            {
+                if (int.TryParse(cookie.Key.Substring("article".Length), out int articleId) &&
+                    int.TryParse(cookie.Value, out int quantity))
+                {
+                    var article = _context.Articles.Include(a => a.Category).FirstOrDefault(a => a.Id == articleId);
+                    if (article != null)
+                    {
+                        cartItems.Add((article, quantity));
+                    }
+                }
+            }
+        }
+
+        return View(cartItems);
+    }
+
+    public IActionResult IncreaseQuantity(int articleId)
+    {
+        string cookieName = $"article{articleId}";
+        if (Request.Cookies.ContainsKey(cookieName))
+        {
+            if (int.TryParse(Request.Cookies[cookieName], out int currentQuantity))
+            {
+                Response.Cookies.Append(cookieName, (currentQuantity + 1).ToString(), new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(7)
+                });
+            }
+        }
+        return RedirectToAction("Cart");
+    }
+
+    public IActionResult DecreaseQuantity(int articleId)
+    {
+        string cookieName = $"article{articleId}";
+        if (Request.Cookies.ContainsKey(cookieName))
+        {
+            if (int.TryParse(Request.Cookies[cookieName], out int currentQuantity) && currentQuantity > 1)
+            {
+                Response.Cookies.Append(cookieName, (currentQuantity - 1).ToString(), new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(7)
+                });
+            }
+            else
+            {
+                Response.Cookies.Delete(cookieName);
+            }
+        }
+        return RedirectToAction("Cart");
+    }
+
+    public IActionResult RemoveFromCart(int articleId)
+    {
+        string cookieName = $"article{articleId}";
+        Response.Cookies.Delete(cookieName);
+        return RedirectToAction("Cart");
+    }
+
+
 }
